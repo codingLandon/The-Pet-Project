537854236dca79d405fe7d95dce0fcc0c6132257
username <ayspina@gmail.com>
2023-01-24 16:04:52 -0500

    Adds contact info to details

--- thepetproject/main_app/forms.py
+++ thepetproject/main_app/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
-from .models import Comment
+from .models import Comment, User
 
 class CommentForm(ModelForm):
   class Meta:
     model = Comment
     fields = ['date', 'content']
+
+class UserForm(ModelForm):
+  class Meta:
+    model = User
+    fields = ['username', 'password', 'email', 'first_name', 'last_name']
--- thepetproject/main_app/views.py
+++ thepetproject/main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.forms import UserCreationForm
 from .models import Resource, Comment
-from .forms import CommentForm
+from .forms import CommentForm, UserForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -39,14 +39,14 @@ def resources_other(request):
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-    form = UserCreationForm(request.POST)
+    form = UserForm(request.POST)
     if form.is_valid():
       user = form.save()
       login(request, user)
       return redirect('/')
     else:
       error_message = 'Invalid sign up - try again'
-  form = UserCreationForm()
+  form = UserForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
