4794d3cc675d42e73d89d5549a82c33cf9dd2eb1
lgrams <laurenrgrams@gmail.com>
2023-01-26 17:15:07 -0500

    edited Procfile

--- thepetproject/thepetproject/wsgi.py
+++ thepetproject/thepetproject/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thepetproject/thepetproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thepetproject.settings')
 
 application = get_wsgi_application()
